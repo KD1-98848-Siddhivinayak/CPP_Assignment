c7947ede00337e6277d3b134623e8366981d0703
Siddhivinayak Gaikwad <gaikwadsiddhivinayak@gmail.com>
2026-08-26 23:36:09 +0530

    Assignment 2 added

---
+++ Assignment 2/assign2_1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+class Valumeofbox {
+    double length;
+    double breadth ;
+    double height;
+    public:
+    Valumeofbox(void){
+        this->length=1;
+        this->breadth=1;
+        this->height=1;
+
+    }
+    Valumeofbox(double length,double breadth,double height){
+        this->length=length;
+        this->breadth=breadth;
+        this->height=height;
+
+    }
+    Valumeofbox(int value ){
+        this->length=value;
+        this->breadth=value;
+        this->height=value;
+
+    }
+
+
+    void printValume(){
+        double val=length*breadth*height;
+        cout<<"valume of box :"<<val;
+
+        
+    }
+
+
+
+};
+
+
+
+int menulist (void ){
+    int choice;
+    cout<<"\n------------------------------------------------";
+    cout <<"\n 1.Calculate Volume with default values \n2.Calculate Volume with length,breadth and height with same value \n3.Calculate Volume with different length,breadth and height values. \n ";
+    cout<<"\n------------------------------------------------\n";
+    cin>>choice;
+    return choice;
+
+}
+int main() {
+
+    int choice;
+    Valumeofbox v1;
+    Valumeofbox v2(10);
+    Valumeofbox v3(10, 20, 30);
+
+    while ((choice = menulist()) != 0) {
+
+        switch (choice) {
+
+            case 1:
+                
+                v1.printValume();
+                break;
+
+            case 2:
+                
+                v2.printValume();
+                break;
+
+            case 3:
+                
+                v3.printValume();
+                break;
+
+            default:
+                cout << "Invalid data";
+        }
+    }
+
+    return 0;
+}
