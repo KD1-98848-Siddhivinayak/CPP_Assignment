6591570331436c39c5123dcfd937a8e381782576
Siddhivinayak Gaikwad <gaikwadsiddhivinayak@gmail.com>
2026-08-28 00:04:13 +0530

    Assignment 3 added

--- Assignment3/assignment3.cpp
+++ Assignment3/assignment3.cpp
@@ -1,50 +1,54 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 class Cylinder {
-    double radius ;
-    double height ;
+    double radius;
+    double height;
     static double pi;
-    public:
-    Cylinder(void):radius(1),height(1){};
-    Cylinder(double radius, double height) :radius(radius),height(height){};
-     void setRadius(double radius){
-        this->radius=radius;
-     } 
-    void  setHeight(double height){
-        this->height=height;
+public:
+    Cylinder(void) :radius(1), height(1) {};
+    Cylinder(double radius, double height) :radius(radius), height(height) {};
+    void setRadius(double radius) {
+        this->radius = radius;
+    }
+    void  setHeight(double height) {
+        this->height = height;
 
-    } 
+    }
     double getRadius() {
         return radius;
     }
-    double getHeight(){
+    double getHeight() {
         return height;
 
     }
     void calculateVolume() {
-        double volume = Cylinder::pi* radius * radius *height;
-        cout<<"--------------------------------"<<endl;
-        cout<<" Valume of Cylinder:"<<volume<<endl;
-        cout<<"--------------------------------"<<endl;
+        double volume = Cylinder::pi * radius * radius * height;
+        cout << "--------------------------------" << endl;
+        cout << " Valume of Cylinder:" << volume << endl;
+        cout << "--------------------------------" << endl;
 
     }
 };
- double Cylinder::pi=3.14;
+double Cylinder::pi = 3.14;
 
 
-int main (){
+int main() {
     Cylinder c1;
+    c1.calculateVolume();
+    Cylinder c3(10, 20);
+    c3.calculateVolume();
+
     c1.setHeight(10);
     c1.setRadius(10);
-    cout<<"radius :"<<c1.getRadius()<<endl;
-    cout<<"height :"<<c1.getHeight()<<endl;
+    cout << "radius :" << c1.getRadius() << endl;
+    cout << "height :" << c1.getHeight() << endl;
     c1.calculateVolume();
-    
+
     Cylinder c2;
     c2.setHeight(30);
     c2.setRadius(40);
-    cout<<"radius :"<<c2.getRadius()<<endl;
-    cout<<"height :"<<c2.getHeight()<<endl;
+    cout << "radius :" << c2.getRadius() << endl;
+    cout << "height :" << c2.getHeight() << endl;
     c2.calculateVolume();
-    
+
 }
